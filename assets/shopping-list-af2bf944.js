2c8167b362267688ff4967224b295d20c63008d4
DeadMakar <130660444+DeadMakar@users.noreply.github.com>
2023-10-15 10:16:34 +0000

    Deploying to gh-pages from @ DeadMakar/Bookkeepers-Project@1acc06e199d600707e8da27b4004c1f58ec527a4 🚀

---
+++ assets/shopping-list-af2bf944.js
@@ -0,0 +1,12 @@
+import{K as i}from"./modal-local-storage-9ae95457.js";const s={ShopUlEl:document.querySelector(".shopping-list-container"),ShopStubEl:document.querySelector(".shopping-list-section"),shopRemoveBtn:document.querySelector(".shopping-delete-btn"),target:document.querySelector(".js-guard")};function t(o){const e=o.map(()=>{`${o.book_image}${o.title}${list_name}${description}${o.author}`}).join("");return s.ShopUlEl.insertAdjacentHTML("beforeend",e),e}function n(){const o=`<div class="shopping-error-wrap">
+      <p class="shopping-error-text">
+        This page is empty, add some books and proceed to order.
+      </p>
+      <img
+        class="shopping-error-img"
+        src="./img/images/oooops-mob-1x.png"
+        alt="books"
+        width="265"
+        height="198"
+      />
+    </div>`;s.ShopStubEl.innerHTML=o}r();function r(){try{const o=JSON.parse(localStorage.getItem(i));if(console.log(o),console.log(o),t(o),o.length===0){n(o);return}}catch(o){console.error("Error:",o)}}
