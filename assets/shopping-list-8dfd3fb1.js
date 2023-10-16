d58d09209177a044b031b91bd7a0449b240427c3
DeadMakar <130660444+DeadMakar@users.noreply.github.com>
2023-10-16 16:29:27 +0000

    Deploying to gh-pages from @ DeadMakar/Bookkeepers-Project@a183409fb50ab4682edaba6ccffa20f7cd5585f9 🚀

--- assets/shopping-list-8dfd3fb1.js
+++ assets/shopping-list-8dfd3fb1.js
@@ -1,4 +1,4 @@
-import{K as r,N as n}from"./modal-local-storage-fb9bb453.js";const t={ShopUlEl:document.querySelector(".shopping-list-container"),ShopStubEl:document.querySelector(".shopping-list-container"),shopRemoveBtnEl:document.querySelector(".shopping-delete-btn"),targetEl:document.querySelector(".js-guard")};function p(i){const o=i.map(e=>`<li class="shopping-item">
+import{K as r,N as n}from"./modal-local-storage-d8cf6053.js";const t={ShopUlEl:document.querySelector(".shopping-list-container"),ShopStubEl:document.querySelector(".shopping-list-container"),shopRemoveBtnEl:document.querySelector(".shopping-delete-btn"),targetEl:document.querySelector(".js-guard")};function p(i){const o=i.map(e=>`<li class="shopping-item">
       <div class="shopping-list-item">
         <div class="shopping-book-overlay">
           <img
